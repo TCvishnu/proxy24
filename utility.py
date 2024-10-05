f829183e7a5bbb430d3cb425decd2d4bf336fda0
Vishnu T C <vishnutc13@gmail.com>
2024-10-06 00:22:14 +0530

    uti

---
+++ utility.py
@@ -0,0 +1,12 @@
+questions = [
+  {"question": "Which command is used to list all files and directories in Linux?", "ans": 1},
+  {"question": "Which command is used to change the current directory in Linux?", "ans": 2},
+  {"question": "What is the command to display the current working directory?", "ans": 3},
+  {"question": "Which command is used to move or rename files in Linux?", "ans": 2},
+  {"question": "What is the command to view the contents of a file in Linux?", "ans": 1},
+  {"question": "Which command is used to remove a file in Linux?", "ans": 2},
+  {"question": "What is the command to create a new directory in Linux?", "ans": 1},
+  {"question": "Which command is used to copy files or directories in Linux?", "ans": 2},
+  {"question": "What is the command to display the manual of any command in Linux?", "ans": 3},
+  {"question": "Which command is used to display the system's network configuration?", "ans": 2}
+]

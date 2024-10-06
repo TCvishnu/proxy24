d6319b4c40094dfd309e4143e9edec28f957c052
Vishnu T C <111764448+TCvishnu@users.noreply.github.com>
2024-10-06 10:20:12 +0530

    Update last.py

--- last.py
+++ last.py
@@ -5,9 +5,16 @@ from timeralso import QuizApp
 from increment_score_player2 import update_player2_score
 from typing import SpeedTrackingApp
 questions = [
-    {"question": "What is 2+2?", "answer": "4"},
-    {"question": "What is the capital of France?", "answer": "Paris"},
-    {"question": "What color is the sky?", "answer": "Blue"}
+  {"question": "You want to list all the files and directories in your current working directory. Which command will you use?", "ans": "ls"},
+  {"question": "You need to navigate to a different directory to work on a project. Which command will allow you to change directories?", "ans": "cd"},
+  {"question": "You are unsure which directory you are currently in. How can you display your present working directory?", "ans": "pwd"},
+  {"question": "You need to move a file to another directory or rename it. Which command should you use?", "ans": "mv"},
+  {"question": "You want to open a file in your directory and view its contents. Which command will allow you to do that?", "ans": "cat"},
+  {"question": "You want to remove a file you no longer need. Which command should you use to delete it?", "ans": "rm"},
+  {"question": "You need to create a new directory for organizing your project files. Which command will you use?", "ans": "mkdir"},
+  {"question": "You want to copy files from one directory to another. Which command will let you copy files?", "ans": "cp"},
+  {"question": "You need help with a specific command and want to check its manual. Which command should you use?", "ans": "man"},
+  {"question": "You want to check the network configuration of your system. Which command will display the network settings?", "ans": "ifconfig"}
 ]
 
 class GameRoomApp(npyscreen.NPSAppManaged):
